bd6568bcc214d2e6965cf770e2e8024459169a03
Ibtisam Akhtar <ibtisamakhtar6@gmail.com>
2024-06-01 22:51:30 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,10 @@
+let nav=document.querySelector(".nav");
+  window.onscroll=function(){
+  if(document.documentElement.scrollTop > 20{
+    nav.classList.add(".header scrolled");
+  }else
+  {
+    nav.classList.remove(".header scrolled");
+
+  }
+  }
